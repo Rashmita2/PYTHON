a50946e28146cdba09fbeaa25b543323e886fa20
Rashmita Khatri <67445848+Rashmita2@users.noreply.github.com>
2021-03-18 16:43:12 -0500

    Add files via upload
    Java programming

---
+++ JAVA/Prepare.java
@@ -0,0 +1,8 @@
+public class Prepare {
+    int x = 5;
+
+    public static void main(String[] args) {
+        Prepare obj1 = new Prepare(); //creates an object
+        System.out.println(obj1.x);
+    }
+}
---
+++ JAVA/Second.java
@@ -0,0 +1,7 @@
+public class Second {
+    public static void main(String[] args) {
+        //Prepare class can be used in another java file
+        Prepare myobj2 = new Prepare();
+        System.out.println(myobj2.x);
+    }
+}
