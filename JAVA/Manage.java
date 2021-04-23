e7a5f0a4f6d03dbccb530b6e966a5d1d7608fd93
Rashmita Khatri <67445848+Rashmita2@users.noreply.github.com>
2021-04-22 20:38:15 -0500

    Create Manage.java

---
+++ JAVA/Manage.java
@@ -0,0 +1,18 @@
+public class Manage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String day = "Friday";
+		switch(day) {
+		case "Sunday":
+			System.out.println("It is Sunday");
+			break;
+		case "Monday":
+			System.out.println("It is Monday");
+			break;
+		default:
+			System.out.println("It is other day");
+				
+		}	
+	}
+}
