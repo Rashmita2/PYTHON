2a0f06900bcd6980426151acbfd9cd46e4fc9988
Rashmita Khatri <67445848+Rashmita2@users.noreply.github.com>
2021-05-14 00:08:20 -0500

    trim start, end and trim

---
+++ C#/trim.cs
@@ -0,0 +1,11 @@
+String greeting = "        Hello        ";
+Console.WriteLine($"[{greeting}]");
+
+string trimmedGreeting = greeting.TrimStart();
+Console.WriteLine($"[{trimmedGreeting}]");
+
+trimmedGreeting = greeting.TrimEnd();
+Console.WriteLine($"[{trimmedGreeting}]");
+
+trimmedGreeting = greeting.Trim();
+Console.WriteLine($"[{trimmedGreeting}]");
