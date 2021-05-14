e5e51eb1818d1498bd7147cb4e913ea92f49676a
Rashmita Khatri <67445848+Rashmita2@users.noreply.github.com>
2021-05-13 23:24:37 -0500

    Create ter.cs

---
+++ C#/ter.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Startapplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool correct = true;
+            int points = correct ? 10 : 0;
+            Console.WriteLine(points);
+        }
+    }
+}
+
+
