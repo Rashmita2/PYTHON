98a699e1d8178bd73082e2489df55c136576483d
Rashmita Khatri <67445848+Rashmita2@users.noreply.github.com>
2021-05-13 22:46:07 -0500

    Create ascii.cs

---
+++ C#/ascii.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace Startapplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 'a';
+            //This prints out the ascii value of a i.e 97
+            Console.WriteLine(number);
+            int num = '\t';
+            Console.WriteLine(num);
+            Console.WriteLine("Hello\tWorld");
+
+        }
+    }
+}
+
+
